b8863af55c0eb30f4e87862827388310ae867ca4
Mikhail <etozhenerk@yandex.ru>
2022-08-05 22:28:10 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ import * as ReactDOMClient from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./store/reducers/rootReducer";
+import thunk from "redux-thunk";
+
+const store = createStore(rootReducer);
 
 const container = document.getElementById("root");
 
-const root = ReactDOMClient.createRoot(container);
+const root = ReactDOMClient.createRoot(container, applyMiddleware(thunk));
 
 const app = (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
 
 root.render(app);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
